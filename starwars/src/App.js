061f821dec4ccf4d530d5cbfa4bbcfbcc1da9f75
royeradames <royeraadames@gmail.com>
2020-07-17 12:20:09 -0400

    added a random number generator

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,6 +11,14 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  //591 total characters
+  // https://rickandmortyapi.com/api/character/[1,2,3]
+  function getRndInteger() {
+    const max = 591
+    const min = 1
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
