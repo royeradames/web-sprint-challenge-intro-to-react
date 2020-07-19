573b6921aa2442fd9ff0cb3893a3d18b7820941f
royeradames <royeraadames@gmail.com>
2020-07-19 08:05:41 -0400

    added a button that refreshes the random characters when click

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,38 +13,32 @@ const App = () => {
   // the state properties here.
 const [characterArr, setCharacterArr] = useState()
 
-function randomCharacters(){
-  const randomCharactersId = [getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger()]
-  const linkToAPI = `https://rickandmortyapi.com/api/character/${randomCharactersId}`
-  debugger
-  axios.get(linkToAPI)
-  .then( (resp) =>{
-    
-    setCharacterArr(resp.data)
-  })
-  .catch((err) =>{
-  })
-}
-const randomCharactersId = [getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger()]
-const linkToAPI = `https://rickandmortyapi.com/api/character/${randomCharactersId}`
-  
+function getRandomCharacters(){
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   //591 total characters
   // https://rickandmortyapi.com/api/character/[1,2,3]
+  const randomCharactersId = [getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger(),getRndInteger()]
+  const linkToAPI = `https://rickandmortyapi.com/api/character/${randomCharactersId}`
+  axios.get(linkToAPI)
+  .then( (resp) =>{
+    setCharacterArr(resp.data)
+  })
+  .catch((err) =>{
+  })
+}
 function getRndInteger() {
     const max = 591
     const min = 1
     return Math.floor(Math.random() * (max - min) ) + min;
   }
 const handleClick = () => {
-  
-  
+  getRandomCharacters()
 }
 useEffect( ()=>{
-  randomCharacters()
+  getRandomCharacters()
 }
 , [])
 
